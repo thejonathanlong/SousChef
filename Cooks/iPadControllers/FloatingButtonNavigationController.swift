041cd9541bbbd7db9d6077fbf62d97f7f64b5dde
Jonathan Long <jonathanelong@icloud.com>
2018-03-08 22:48:26 -0800

    - Fixed StackView animating from the top - Added Bug Report Button

--- Cooks/iPadControllers/FloatingButtonNavigationController.swift
+++ Cooks/iPadControllers/FloatingButtonNavigationController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import MessageUI
 
-class FloatingButtonNavigationController: UINavigationController, UINavigationControllerDelegate {
+class FloatingButtonNavigationController: UINavigationController, UINavigationControllerDelegate, MFMailComposeViewControllerDelegate {
 
 	let floatingButtonStack = UIStackView()
 	let backButton = SousChefButton(frame: .zero)
+	let bugButton = SousChefButton(frame: .zero)
 	// Without the dummyView the stackView animates weird when hiding and unhiding the buttons.
 	let dummyView = UIView()
 	
@@ -50,19 +52,32 @@ class FloatingButtonNavigationController: UINavigationController, UINavigationCo
 		backButton.setImage(backButtonImage, for: .normal)
 		backButton.addTarget(self, action: #selector(goBack), for: .primaryActionTriggered)
 		backButton.translatesAutoresizingMaskIntoConstraints = false
+		backButton.isHidden = true
+		
+		let bugButtonImage = UIImage(named: "Bug")!
+		bugButton.setImage(bugButtonImage, for: .normal)
+		bugButton.addTarget(self, action: #selector(reportBug), for: .primaryActionTriggered)
+		bugButton.translatesAutoresizingMaskIntoConstraints = false
 		
 		dummyView.translatesAutoresizingMaskIntoConstraints = false
 		
 		floatingButtonStack.addArrangedSubview(dummyView)
 		floatingButtonStack.addArrangedSubview(backButton)
-		view.addSubview(floatingButtonStack)
+		floatingButtonStack.addArrangedSubview(bugButton)
 		
+		view.addSubview(floatingButtonStack)
+
 		let constraint = [
 			floatingButtonStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: SousChefStyling.standardMargin),
 			floatingButtonStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -SousChefStyling.standardMargin),
+			floatingButtonStack.heightAnchor.constraint(equalToConstant: SousChefStyling.navigationFloatingButtonHeight),
 			dummyView.widthAnchor.constraint(equalToConstant: 1),
+			dummyView.heightAnchor.constraint(greaterThanOrEqualToConstant: 1),
 			backButton.widthAnchor.constraint(equalToConstant: SousChefStyling.navigationFloatingButtonWidth),
-			backButton.heightAnchor.constraint(equalToConstant: SousChefStyling.navigationFloatingButtonHeight)
+			backButton.heightAnchor.constraint(equalToConstant: SousChefStyling.navigationFloatingButtonHeight),
+			bugButton.widthAnchor.constraint(equalToConstant: SousChefStyling.navigationFloatingButtonWidth),
+			bugButton.heightAnchor.constraint(equalToConstant: SousChefStyling.navigationFloatingButtonHeight),
+			
 		]
 		
 		NSLayoutConstraint.activate(constraint)
@@ -83,17 +98,10 @@ class FloatingButtonNavigationController: UINavigationController, UINavigationCo
 		return nil
 	}
 	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-//		let pleaseSignIntoTheCloudAlertController = UIAlertController(title: "Please Sign into iCloud", message: "In order for Sous Chef to work properly you need to be signed into iCloud. You can do this in System Preferences.", preferredStyle: .alert)
-//		let isSignedIntoTheCloud = FileManager.default.ubiquityIdentityToken != nil
-//		if !isSignedIntoTheCloud {
-////			pushViewController(pleaseSignIntoTheCloudAlertController, animated: true)
-//			topViewController?.present(pleaseSignIntoTheCloudAlertController, animated: true, completion: nil)
-//		} else if presentedViewController == pleaseSignIntoTheCloudAlertController {
-////			popViewController(animated: true)
-//			pleaseSignIntoTheCloudAlertController.dismiss(animated: true, completion: nil)
-//		}
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		// Without this call the stackView animates in from the top left...
+		self.viewIfLoaded?.layoutIfNeeded()
 	}
 }
 
@@ -154,17 +162,58 @@ extension FloatingButtonNavigationController {
 	}
 }
 
-// Button Actions
+// MARK: - Button Actions
 extension FloatingButtonNavigationController {
 	@objc func goBack() {
-		popViewController(animated: true)
+		let _ = popViewController(animated: true)
+	}
+	
+	@objc func reportBug() {
+		if MFMailComposeViewController.canSendMail() {
+			let mailComposeViewController = MFMailComposeViewController()
+			let timeInterval = NSDate().timeIntervalSince1970
+			mailComposeViewController.setSubject("Bug SC://\(timeInterval)")
+			mailComposeViewController.setToRecipients(["sue.souschef@gmail.com"])
+			mailComposeViewController.mailComposeDelegate = self
+			if let screenCap = screenshot(), let screenCapData = UIImageJPEGRepresentation(screenCap, 1.0) {
+				mailComposeViewController.addAttachmentData(screenCapData, mimeType: "image/jpeg", fileName: "screenCap-\(timeInterval).jpg")
+			}
+			
+			if let topVC = topViewController {
+				topVC.present(mailComposeViewController, animated: true, completion: nil)
+			}
+		}
+		else {
+		}
+	}
+	
+	//Utility
+	func screenshot() -> UIImage? {
+		if let topViewController = topViewController {
+			let size = topViewController.view.frame.size
+			UIGraphicsBeginImageContextWithOptions(size, false, UIScreen.main.scale)
+			topViewController.view.drawHierarchy(in: topViewController.view.frame, afterScreenUpdates: true)
+			let image = UIGraphicsGetImageFromCurrentImageContext()
+			UIGraphicsEndImageContext()
+			
+			return image
+		}
+		
+		return nil
+	}
+}
+
+// MARK: - MFMailComposeViewControllerDelegate
+extension FloatingButtonNavigationController {
+	func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+		controller.dismiss(animated: true, completion: nil)
 	}
 }
 
 // MARK: - UINavigationControllerDelegate
 extension FloatingButtonNavigationController {
 	func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
-		UIView.animate(withDuration: 0.33) {
+		UIView.animate(withDuration: 0.1) {
 			self.backButton.isHidden = self.viewControllers.count <= 1
 			self.view.layoutIfNeeded()
 		}
